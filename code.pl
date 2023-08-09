57799cf91ed878ce2701b8e42a780f317c35cd08
DanteBonino <43711055+DanteBonino@users.noreply.github.com>
2023-08-09 16:36:19 -0300

    Puntos 2, 3 y 4

--- code.pl
+++ code.pl
@@ -46,3 +46,47 @@ peligroso(Personaje):-
 peligrosaActividad(mafioso(maton)).
 peligrosaActividad(ladron(SitiosDeRobo)):-
     member(licorerias,SitiosDeRobo).
+
+%Punto 2:
+duoTemible(UnPersonaje, OtroPersonaje):-
+    esPeligroso(UnPersonaje),
+    esPeligroso(OtroPersonaje),
+    duo(UnPersonaje, OtroPersonaje).
+
+duo(Personaje, OtroPersonaje):-
+    algunoEsAmigo(Personaje, OtroPersonaje).
+duo(Personaje,OtroPersonaje):-
+    algunoEsPareja(Personaje, OtroPersonaje).
+
+algunoEsPareja(Personaje, OtroPersonaje):-
+    pareja(Personaje,OtroPersonaje).
+algunoEsPareja(Personaje, OtroPersonaje):-
+    pareja(OtroPersonaje, Personaje).
+
+algunoEsAmigo(Personaje, OtroPersonaje):-
+    amigo(Personaje,OtroPersonaje).
+algunoEsAmigo(Personaje, OtroPersonaje):-
+    amigo(OtroPersonaje, Personaje).
+
+%Punto 3:
+estaEnProblemas(Personaje):-
+    encargoProblematico(Personaje).
+
+encargoProblematico(Personaje):-
+    trabajaPara(Empleador, Personaje),
+    esPeligroso(Empleador),
+    pareja(Empleador, Pareja),
+    encargo(Empleador, Personaje, cuidar(Pareja)).
+encargoProblematico(Personaje):-
+    encargo(_,Personaje, buscar(PersonaBuscada,_)),
+    personaje(PersonaBuscada, boxeador).
+
+%Punto 4:
+sanCayetano(Personaje):-
+    personaje(Personaje,_),
+    forall(personaCercana(Personaje, PersonaCercana), encargo(Personaje, PersonaCercana,_)).
+%Habría que ver si sanCayetano tiene que tener al menos una persona cercana, pq si no también cuenta las personas que no tienen nadie cercano.
+personaCercana(Persona, PersonaCercana):-
+    algunoEsAmigo(Persona, PersonaCercana).
+personaCercana(Persona, PersonaCercana):-
+    trabajaPara(Persona, PersonaCercana).
